e303217bef6716540adb4e08d359a8f01a54645f
Yarden Sade <yarden@jit.io>
2023-01-17 15:54:36 +0200

    Update a.py

--- a.py
+++ a.py
@@ -10,4 +10,4 @@ x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=Tru
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
 wow = 1
-wow = 5
+wow = 6
