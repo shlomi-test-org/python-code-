a5f0f20eddd183afebd698f3c705f612a181f913
Yarden Sade <yarden@jit.io>
2023-01-17 13:50:38 +0200

    Update a.py

--- a.py
+++ a.py
@@ -9,3 +9,4 @@ x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=Tru
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
+wow = 1
