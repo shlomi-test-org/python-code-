28d70fa2289ef44a0ab8239f0f2197f14ecd62f2
Yarden Sade <yarden@jit.io>
2023-01-30 13:58:52 +0200

    Update a.py

--- gg/a.py
+++ gg/a.py
@@ -11,7 +11,7 @@ x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=Tru
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
 wow = 1223dw
-567sdfffs
+567sdfffsasdasd
 dfgrdדגכדגכע
 ff
 ק
