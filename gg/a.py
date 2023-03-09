1eae347714a0d56f4e3e0b608d87aacb0e2710ef
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-09 15:31:03 +0200

    Update a.py

--- gg/a.py
+++ gg/a.py
@@ -11,3 +11,7 @@ wow = 1
 
 
 x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
+x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
+x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
+x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
+x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
