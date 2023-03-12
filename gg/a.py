3227d4c3be2e8d1b21cf4900faa07ac63aadfc6c
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-12 15:17:49 +0200

    Update a.py

--- gg/a.py
+++ gg/a.py
@@ -8,5 +8,4 @@ x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=Tru
 wow = 1
 
 
-
-
+x_2022_02_26_1 = subprocess.check_output(f"nslookup2 {my_new_domain}", shell=True, encoding='UTF-8')
