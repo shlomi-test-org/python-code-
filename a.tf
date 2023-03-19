14e335195b69c5fe27e4ec26d09132a5f1378d53
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-19 10:40:52 +0200

    Update a.tf

--- a.tf
+++ a.tf
@@ -19,6 +19,7 @@ resource "aws_redshift_cluster" "positive1" {
   cluster_type       = "single-node"
 }
 
+
 resource "aws_redshift_cluster" "positive2" {
   cluster_identifier = "tf-redshift-cluster"
   database_name      = "mydb"
