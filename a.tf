5632865326ea34e4d68e0d07df6d931b6571f1c8
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-19 11:21:13 +0200

    Update a.tf

--- a.tf
+++ a.tf
@@ -13,6 +13,7 @@ ingress {
 resource "aws_redshift_cluster" "positive1" {
   cluster_identifier = "tf-redshift-cluster"
   database_name      = "mydb"
+  
   master_username    = "foo"
   master_password    = "Mustbe8characters"
   node_type          = "dc1.large"
