3e17c66f8e280ed1bea9770bd8699d9cb4daebcf
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-27 09:18:31 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -82,6 +82,12 @@ self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").rea
                       
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+                      
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
                       
