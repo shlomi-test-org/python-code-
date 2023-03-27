0b5c3feece07689510440dae7de5629fbb3e147f
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-27 08:58:07 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -144,6 +144,15 @@ self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").rea
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+                      
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
                       
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
