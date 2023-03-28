9b7690cb1b424c8fb746e4e23ca3e390db4d7be0
Yarden Sade <yarden@jit.io>
2023-03-28 16:54:48 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -185,6 +185,7 @@ output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF
                       
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
-
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
 aws_secret = "AKIAIMNOJVGFDXXXE4OA"
