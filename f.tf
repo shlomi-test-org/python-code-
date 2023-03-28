713bcfb2e5777a5520cb1260b1a790bb5b10cb0f
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-03-28 12:10:32 +0300

    Update f.tf

--- f.tf
+++ f.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_public_access_block" "positive3" {
 resource "aws_s3_bucket_public_access_block" "positive3" {
   bucket = aws_s3_bucket.example.id
 
-  block_public_policy = true
-  ignore_public_acls  = true
+  block_public_policy = false
+  ignore_public_acls  = false
   restrict_public_buckets = false
 }
