3372d95d42294cf6250d1e20886371cd73eff336
Yarden Sade <yarden@jit.io>
2023-03-28 17:06:03 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -1,4 +1,4 @@
-another_var = "a"
+another_var = "ab"
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
