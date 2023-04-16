388554e4b32ff2b575aa669b866e903d6772b34f
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-04-16 09:42:49 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -9,7 +9,8 @@ output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
                       
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", sh
+                                ell=True, encoding='UTF-8')
 
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
@@ -187,6 +188,7 @@ output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
 aws_secret = "AKIAIMNOJVGFDXXXE4OA"
