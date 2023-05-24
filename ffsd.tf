a02680f12e72e25eb546e824e405c8357cdf205b
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-05-24 15:55:44 +0300

    Update ffsd.tf

--- ffsd.tf
+++ ffsd.tf
@@ -5,6 +5,7 @@ resource "aws_redshift_cluster" "positive1" {
   
   master_username    = "foo"
   master_password    = "Mustbe8characters"
+  
   node_type          = "dc1.large"
   cluster_type       = "single-node"
 }
