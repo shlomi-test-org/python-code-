4d655c70efbc5e271c4e6747a9d0f70e0500d7b9
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-05-30 14:04:36 +0300

    Update ffsd.tf

--- ffsd.tf
+++ ffsd.tf
@@ -16,6 +16,7 @@ resource "aws_redshift_cluster" "positive1" {
 resource "aws_redshift_cluster" "positive2" {
   cluster_identifier = "tf-redshift-cluster"
   
+  
   database_name      = "mydb"
   master_username    = "foo"
   master_password    = "Mustbe8characters"
