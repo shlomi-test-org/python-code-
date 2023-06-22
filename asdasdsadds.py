13742534081c82d503e4296b846541bcd105efa1
Yarden Sade <yarden@jit.io>
2023-06-22 10:47:54 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -1,5 +1,6 @@
 another_var = "a"
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
