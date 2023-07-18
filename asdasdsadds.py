b734d3011ce8f213a555592b23b062518879a131
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-07-18 14:08:57 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -1,198 +1,85 @@
 another_var = "a"
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-aws_secret = "AKIAIMNOJVGFDXXXE4OA"
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-                      
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
-
-aws_secret = "AKIAIMNOJVGFDXXXE4OA"
 aws_secret = "AKIAIMNOJVGFDXXXE4OB"
-aws_secret = "AKIAIMNOJVGFDXXXE4OCX"
+
+
+
+
+
+
+self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
