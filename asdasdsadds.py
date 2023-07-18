5279a9ecdde72b0fdee27b71cf6d3a6d3124acd5
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-07-18 14:12:03 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -6,7 +6,7 @@ aws_secret = "AKIAIMNOJVGFDXXXE4OB"
 
 
 
-self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 another_var = "a"
 another_var = "a"
 another_var = "a"
