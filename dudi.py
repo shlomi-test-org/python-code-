1956772d892276c5f7509cde3e4d7278511d833c
Yarden Sade <yarden@jit.io>
2023-07-18 14:09:00 +0300

    Create dudi.py

---
+++ dudi.py
@@ -0,0 +1,21 @@
+wow
+
+ma 
+
+
+kore
+
+
+
+yo
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+
+
+
+shtak
+
+
+hi
