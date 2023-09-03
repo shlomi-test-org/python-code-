d9ca628ba641f483fc3e44fc15964d734bc13014
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-09-03 14:42:01 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -16,6 +16,9 @@ aws_secret = "AKIAIMNOJVGFDXXXE4OB"
 
 
 
+
+
+
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
