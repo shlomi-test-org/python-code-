f81b885de4cdb64630188555acc351b3dbb5b637
adiattlan <104570236+adiattlan@users.noreply.github.com>
2023-09-21 19:11:04 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -11,6 +11,14 @@ output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF
 
 
 
+
+
+
+
+
+
+
+
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 another_var = "a"
 another_var = "a"
