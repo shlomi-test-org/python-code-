125a5d1ae85e1e6973b288c9c13df561176ede99
eyaliyahu <99614870+eyaliyahu@users.noreply.github.com>
2024-02-04 16:31:42 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
 # flake8: noqa
-a = "ASIAAQWSEDRFTGYHUJUJ"
+a = "ASIAAQWSEDRFTGYHUJUJa"
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 output_2 = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
