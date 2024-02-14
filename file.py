f2d2b8474294706e8f6024358a341cef51bb3e14
adiattlan <104570236+adiattlan@users.noreply.github.com>
2024-02-14 14:38:47 +0200

    Update file.py

--- file.py
+++ file.py
@@ -2,3 +2,9 @@
 a = "ASIAAQWSEDRFTGYHUJUJ"
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 dasdasdasd
+
+a = "ASIAAQWSEDRFTGYHUJUJ"
+a = "ASIAAQWSEDRFTGYHUJUJ"
+a = "ASIAAQWSEDRFTGYHUJUJ"
+a = "ASIAAQWSEDRFTGYHUJUJ"
+a = "ASIAAQWSEDRFTGYHUJUJ"
