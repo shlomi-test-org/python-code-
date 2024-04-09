766fc99dd80401833fe64b617baf80a92ec09944
adiattlan <104570236+adiattlan@users.noreply.github.com>
2024-04-09 14:51:03 +0300

    Create a.py

---
+++ a.py
@@ -0,0 +1,260 @@
+another_var = "aa"
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+another_var = "aa"
+aws_secret = "AKIAIMNOJVGFDXXXE4OB"
+
+
+
+
+
+assert eyal == 1
+
+another_var = "a"
+
+
+\sldkjflkdajglksdlkjsdg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = "ASIAAQWSEDRFTGYHUJUJ"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+assert eyal == 1
+
+another_var = "a"
+
+
+\sldkjflkdajglksdlkjsdg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = "ASIAAQWSEDRFTGYHUJUJ"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+
+
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
+another_var = "a"
