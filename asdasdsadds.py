c70c95c947129769f10bb1cb3889f6f30cad9e3b
Yarden Sade <yarden@jit.io>
2024-05-01 19:05:33 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -4,6 +4,8 @@ aws_secret1 = "AKIAIMNOJVGFDXXXE4OC"
 aws_secret1 = "AKIAIMNOJVGFDXXXE4OD"
 aws_secret1 = "AKIAIMNOJVGFDXXXE4OD"
 aws_secret1 = "AKIAIMNOJVGFDXXXE4OD"
+aws_secret1 = "AKIAIMNOJVGFDXXXE4OD"
+aws_secret1 = "AKIAIMNOJVGFDXXXE4OD"
 
 
 another_var = "aa"
