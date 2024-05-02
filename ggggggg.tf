67b3b8e0e9263114e15ea8b25318d08590759f4d
JonathanRosenboim <101969613+JonathanRosenboim@users.noreply.github.com>
2024-05-02 09:42:18 +0300

    Update ggggggg.tf

--- ggggggg.tf
+++ ggggggg.tf
@@ -3,3 +3,9 @@ resource "aws_s3_bucket" "example_bucket" {
   acl           = "public-read-write"
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "example_bucket2" {
+  bucket        = "my-tf-test-bucket"
+  acl           = "public-read-write"
+  force_destroy = true
+}
