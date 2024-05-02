b86ab078b4d06d4408a6dbb0616c5cc5691f99bf
avichayJit <94782810+avichayJit@users.noreply.github.com>
2024-05-02 11:55:02 +0300

    Fixing PR

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -46,7 +46,7 @@ another_var = "a"
 
 
 
-output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+output = subprocess.check_output(f"nslookup {domain}", shell=False, encoding='UTF-8')
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
