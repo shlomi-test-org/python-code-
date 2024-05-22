e1633e24884226613c87562a88b95dc0552bd11d
Adi Attlan <adidog2269@gmail.com>
2024-05-22 08:44:46 +0300

    Update ffsd.tf

--- ffsd.tf
+++ ffsd.tf
@@ -4,7 +4,6 @@ resource "aws_redshift_cluster" "positive1" {
   database_name      = "mydb"
   
   master_username    = "foo"
-  master_password    = "Mustbe8characters"
   node_type          = "dc1.large"
   cluster_type       = "single-node"
 }
@@ -15,7 +14,6 @@ resource "aws_redshift_cluster" "positive2" {
   cluster_identifier = "tf-redshift-cluster"
   database_name      = "mydb"
   master_username    = "foo"
-  master_password    = "Mustbe8characters"
   node_type          = "dc1.large"
   cluster_type       = "sinaaaglt-node"
   encrypted          = true
