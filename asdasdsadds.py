0af8fcf99dd826497b7d8a0334219c49c8989348
Adi Attlan <adidog2269@gmail.com>
2024-08-13 09:26:54 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -6,3 +6,11 @@
                       
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 self.logs += os.popen(f"git remote add origin {self.repo_instance.ssh_url}").read()
+
+
+
+
+
+
+
+
