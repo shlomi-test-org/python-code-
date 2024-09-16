03af8d58cec320f6d4665143cbeaa5090f40fa62
Adi Attlan <adidog2269@gmail.com>
2024-09-16 15:51:38 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,5 @@
 output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+aws_secret1 = "AKIAIMNOJVGFDXXXE4OC"
+
+
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
