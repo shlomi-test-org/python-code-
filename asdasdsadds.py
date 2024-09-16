70525c35f6ca5ab81c42280abd6d7f2825df5389
Adi Attlan <adidog2269@gmail.com>
2024-09-16 13:11:15 +0300

    Update asdasdsadds.py

--- asdasdsadds.py
+++ asdasdsadds.py
@@ -17,7 +17,7 @@ aws_secret1 = "AKIAIMNOJVGFDXXXE4OC"
 
 
 aws_secret1 = "AKIAIMNOJVGFDXXXE4OC"
-aws_secret1 = "AKIAIMNOJVGFDXXXE4OC"
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
 
 
 
