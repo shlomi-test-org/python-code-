b6089b5e006020160f04f23071a14415a57f4046
Adi Attlan <adidog2269@gmail.com>
2025-05-14 13:40:54 +0300

    Update a.py

--- a.py
+++ a.py
@@ -5538,6 +5538,9 @@ output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
+output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
+output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
+output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
 aws_secret1 = "AKIAIMNOJVGFDXXXE4OC"
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
 output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8')
